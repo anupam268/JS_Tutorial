31d735b195849a7b8c4dd94fab87ef3f4fbb9c61
Anupam Anand <53298698+anupam268@users.noreply.github.com>
2024-02-24 14:44:42 +0000

    datatypes lecture completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use_strict"; //treat all the JS code as newer version
+
+// alert( 3+3 )   // we are using node.js not browser
+
+
+console.log( 3+3);
+ console.log("anupam");
+
+
+let name = "anupam"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof state);
+console.log(typeof isLoggedIn);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JS_Tutorial
+
+### References:
+
+01. ECMA mdn --> https://developer.mozilla.org/en-US/docs/Glossary/ECMAScript
+02. tc39. --> https://tc39.es/
